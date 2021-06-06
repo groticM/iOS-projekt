d742a88bfd1985aec8b24c77b225a01cbe75a360
five <lucija.omrcen@fer.hr>
2021-06-06 11:58:13 +0200

    first commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by five on 06/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
